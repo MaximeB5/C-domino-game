a0ff39f579a678acbb68c2a379dd172d09e6641a
MaximeB5 <belavalmax@sfr.fr>
2021-01-28 11:05:30 +0100

    add minimal stuff to begin the project

---
+++ src/main.c
@@ -0,0 +1,16 @@
+// My Includes
+	// None for the moment.
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// My Defines
+	// None for the moment.
+
+int main()
+{
+	printf("Hello from C-Domino.\n");
+
+	return(0);
+}
